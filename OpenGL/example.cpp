b0c8b4d59f81cafd3ff492f6d45abf31319a7cc1
yuweichen1008 <yuweiichen@gmail.com>
2018-07-12 19:41:47 +0800

    Upload example call for OpenGL texture

--- OpenGL/example.cpp
+++ OpenGL/example.cpp
@@ -1,9 +1,86 @@
 //#pragma warning(disable 4996)
+#include <GL/glew.h>
 #include <GL/glut.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-GLuint LoadTexture(const char * filename)
+
+void DrawImage(GLuint in_texture) {
+
+	glMatrixMode(GL_PROJECTION);
+	glPushMatrix();
+	glLoadIdentity();
+	glOrtho(0.0, glutGet(GLUT_WINDOW_WIDTH), 0.0, glutGet(GLUT_WINDOW_HEIGHT), -1.0, 1.0);
+	glMatrixMode(GL_MODELVIEW);
+	glPushMatrix();
+
+
+	glLoadIdentity();
+	glDisable(GL_LIGHTING);
+
+
+	glColor3f(1, 1, 1);
+	glEnable(GL_TEXTURE_2D);
+	glBindTexture(GL_TEXTURE_2D, in_texture);
+
+
+	// Draw a textured quad
+	glBegin(GL_QUADS);
+	glTexCoord2f(0, 0);
+	glVertex2i(-1, -1);
+	glTexCoord2f(1, 0);
+	glVertex2i(1, -1);
+	glTexCoord2f(1, 1);
+	glVertex2i(1, 1);
+	glTexCoord2f(0, 1);
+	glVertex2i(-1, 1);
+	glEnd();
+
+
+	glDisable(GL_TEXTURE_2D);
+	glPopMatrix();
+
+
+	glMatrixMode(GL_PROJECTION);
+	glPopMatrix();
+
+	glMatrixMode(GL_MODELVIEW);
+
+}
+
+
+GLuint texture = 0;
+void display()
+{
+	glClearColor(0, 0, 0, 1);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(-2, 2, -2, 2, 2, -2);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	glEnable(GL_TEXTURE_2D);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
+
+	glBegin(GL_QUADS);
+	glTexCoord2f(0, 0);
+	glVertex2i(-1, -1);
+	glTexCoord2f(1, 0);
+	glVertex2i(1, -1);
+	glTexCoord2f(1, 1);
+	glVertex2i(1, 1);
+	glTexCoord2f(0, 1);
+	glVertex2i(-1, 1);
+	glEnd();
+
+	glutSwapBuffers();
+}
+
+GLuint loadTexture(const char * filename)
 {
 
 	GLuint texture;
@@ -56,7 +133,8 @@ GLuint LoadTexture(const char * filename)
 
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 	
 	free(data);
 	return texture;
@@ -67,11 +145,28 @@ int main(int argc, char* argv[])
 	printf("[main]initialize the code here...\n");
 
 	//gluBuild2DMipmaps(GL_TEXTURE_2D, 3, width, height, GL_RGB, GL_UNSIGNED_BYTE, &data);
+	/*
+	GLuint texture;
+	texture = loadTexture(argv[1]);
 
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+	glutInitWindowSize(512, 512);
+	glutCreateWindow("glutTest09");
+
+	DrawImage(texture);
+	*/
 	// Poor filtering. Needed !
 	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);
+	glutInitWindowSize(640, 480);
+	glutCreateWindow("GLUT");
+	glewInit();
+	texture = loadTexture(argv[1]);
+	glutDisplayFunc(display);
+	glutMainLoop();
+	return 0;
 
 	printf("[main]finish here.\n");
 	system("pause");
