65d7b1d9646f3e036174e8234f25de221538f728
yuweichen1008 <yuweiichen@gmail.com>
2018-07-15 10:09:07 +0800

    Upload the working version

--- Xplane_Radar/main.cpp
+++ Xplane_Radar/main.cpp
@@ -1,18 +1,56 @@
-//#pragma warning(disable 4996)
-#include <GL/glut.h>
+#pragma warning(disable 4996)
+
+#include <GL\glut.h>
+#include <GL\GL.h>
+#include <GL\freeglut_ext.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-GLuint LoadTexture(const char * filename)
-{
+//#pragma comment( lib, "glew32.lib" )
 
-	GLuint texture;
-	unsigned int width, height;
-	unsigned char * data;
+unsigned int width = 480, height = 640;
+GLuint texture = 0;
+
+/* Handler for window-repaint event. Called back when the window first appears and
+whenever the window needs to be re-painted. */
+void display()
+{
+	// Clear color and depth buffers
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glMatrixMode(GL_MODELVIEW);     // Operate on model-view matrix
+
+									/* Draw a quad */
+	glBegin(GL_QUADS);
+	glTexCoord2i(0, 0); glVertex2i(0, 0);
+	glTexCoord2i(0, 1); glVertex2i(0, height);
+	glTexCoord2i(1, 1); glVertex2i(width, height);
+	glTexCoord2i(1, 0); glVertex2i(width, 0);
+	glEnd();
+
+	glutSwapBuffers();
+}
+
+/* Handler for window re-size event. Called back when the window first appears and
+whenever the window is re-sized with its new width and height */
+void reshape(GLsizei newwidth, GLsizei newheight)
+{
+	// Set the viewport to cover the new window
+	glViewport(0, 0, width = newwidth, height = newheight);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(0.0, width, height, 0.0, 0.0, 100.0);
+	glMatrixMode(GL_MODELVIEW);
+
+	glutPostRedisplay();
+}
+
+//loadTexture .bmp 24bit RGB image function
+GLuint loadTexture(const char * filename)
+{
 	unsigned char header[54]; // Each BMP file begins by a 54-bytes header
 	unsigned int dataPos;     // Position in the file where the actual data begins
 	unsigned int imageSize;   // width*height*3
-	// Read file
+							  // Read file
 	FILE * file;
 	errno_t err;
 
@@ -23,7 +61,7 @@ GLuint LoadTexture(const char * filename)
 		return false;
 	}
 	// check the bmp file
-	if (fread(header, 1, 54, file) != 54) { 
+	if (fread(header, 1, 54, file) != 54) {
 		printf("Not a correct BMP file\n");
 		return false;
 	}
@@ -40,38 +78,93 @@ GLuint LoadTexture(const char * filename)
 	// Some BMP files are misformatted, guess missing information
 	if (imageSize == 0)    imageSize = width*height * 3; // 3 : one byte for each Red, Green and Blue component
 	if (dataPos == 0)      dataPos = 54; // The BMP header is done that way
-	
-	// Create a buffer
-	data = (unsigned char *)malloc(imageSize);
-
+										 
+	//data = (unsigned char *)malloc(imageSize);// Create a buffer
+	unsigned char* data = new unsigned char[imageSize];
 	// Read the actual data from the file into the buffer
-	fread(data, width * height * 3, 1, file);
-
-	fclose(file);
+	// fread ( void * ptr, size_t size, size_t count, FILE * stream );
+	fread(data, sizeof(unsigned char), imageSize, file);
+	
+	
+	// Convert (B, G, R) to (R, G, B)
+	unsigned char tmp;
+	for (int j = 0; j < width * 3; j += 3)
+	{
+		tmp = data[j];
+		data[j] = data[j + 2];
+		data[j + 2] = tmp;
+	}
+	GLuint texture;
 	glGenTextures(1, &texture);
 	// "Bind" the newly created texture : all future texture functions will modify this texture
 	glBindTexture(GL_TEXTURE_2D, texture);
 	// Give the image to OpenGL
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_BGR_EXT, GL_UNSIGNED_BYTE, data);
 
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 	
-	
+	// Release memory
+	fclose(file);
 	free(data);
 	return texture;
+}
+
+void initGL()
+{
+	glViewport(0, 0, width, height); // use a screen size of width by height
+	glEnable(GL_TEXTURE_2D);     // Enable 2D texturing
+
+	glMatrixMode(GL_PROJECTION);     // Make a simple 2D projection on the entire window
+	glLoadIdentity();
+	
+	//glOrtho(0.0, w, h, 0.0, 0.0, 100.0);
+	//glOrtho(0.0, glutGet(GLUT_WINDOW_WIDTH), 0.0, glutGet(GLUT_WINDOW_HEIGHT), -1.0, 1.0);
+	glOrtho(0.0, width, 0.0, height, 0.0, 1.0);
+
+	glMatrixMode(GL_MODELVIEW);    // Set the matrix mode to object modeling
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);	// Set default
+	glClearDepth(0.0f);						// Set default
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Clear the window
+	
 }
 
 int main(int argc, char* argv[])
 {
 	printf("[main]initialize the code here...\n");
 
-	//gluBuild2DMipmaps(GL_TEXTURE_2D, 3, width, height, GL_RGB, GL_UNSIGNED_BYTE, &data);
+	/*
+	GLuint texture;
+	texture = loadTexture(argv[1]);
 
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+	glutInitWindowSize(512, 512);
+	glutCreateWindow("glutTest09");
+
+	DrawImage(texture);
 	// Poor filtering. Needed !
 	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 
+	*/
+
+	/* GLUT init */
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGB);
+	glutInitWindowSize(width, height);
+	glutCreateWindow(argv[1]);	// Set windows name
+	glutDisplayFunc(display);
+	glutReshapeFunc(reshape);	// Optional
+
+	/* OpenGL 2D generic initialization*/
+	initGL();
+	// load image with loadTexture function
+	texture = loadTexture(argv[1]);
+
+	/* OpenGL main loop*/
+	glutMainLoop();
 
 	printf("[main]finish here.\n");
 	system("pause");
