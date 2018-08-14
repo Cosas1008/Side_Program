c0a62c6142565a7c06a2268eac5c4cf00fc14597
yuweichen1008 <yuweiichen@gmail.com>
2018-08-14 09:56:01 +0800

    Random sweep function added

--- Xplane_Radar/Linux/radar.cpp
+++ Xplane_Radar/Linux/radar.cpp
@@ -25,7 +25,7 @@
 const int n = 20;
 const GLfloat R = 1.0f;
 const GLfloat Pi = 3.1415926536f;
-static int day = 200;
+float randomAng = 0.0f;
 double FPS, FPS_old;
 char fpsChar[255];
 // Function Declair
@@ -33,6 +33,7 @@ double CalFrequency(void);
 void myDisplay(void);
 void myIdle(void);
 void print(int x, int y,int z, char *string);
+void drive();
 
 void myDisplay(void)
 {
@@ -40,22 +41,7 @@ void myDisplay(void)
     glClearColor(0.0, 0.0, 0.0, 0.0);
     glClear(GL_COLOR_BUFFER_BIT);
     int n = 120;
-
-    // float da, ga, gb, a, b;
-    // da = 2*Pi / (float)n;
-    // glBegin(GL_QUADS);
-	// for (int i = 0; i < n; i++) {
-	// 	a = (float)i * da;
-	// 	b = a + da;
-	// 	ga = 0.90 * (float)(i) / (float)n;
-	// 	gb = 0.90 * (float)(i + 1) / (float)n;
-	// 	glColor3f(0, ga, 0);
-	// 	glVertex2i(0, 0); glVertex2f(cos(a), sin(a));
-	// 	glColor3f(0, gb, 0);
-	// 	glVertex2f(cos(b), sin(b)); glVertex2i(0, 0);
-	// }
-	// glEnd();
-
+    float range = 0.01f;
     glLineWidth(1.0f);
     glViewport(0,-HEIGHT/2,WIDTH,HEIGHT);
     glBegin(GL_LINE_LOOP);
@@ -66,15 +52,31 @@ void myDisplay(void)
         }
     }
     glEnd();
+    // sweep range
+    glBegin(GL_QUADS);
+    glColor4f(0.0f,0.6f,0.0f,0.3f);
+    glVertex2i(0,0);
+    glVertex2f( R*cos(Pi*(randomAng + range)) , R*sin(Pi*(randomAng + range)));
+    glVertex2f( R*cos(Pi*(randomAng - range)) , R*sin(Pi*(randomAng - range)));
+    glVertex2i(0,0);
+    glEnd();
+
     glFlush();
     glutSwapBuffers();
 }
 
-void printingOut(char name[])
+void drive(int data)
 {
-    int i = 0;
-    while(i <= strlen(name) ){
-        std::cout << name[i];
+    glutTimerFunc(50, drive, -1);// call drive() again in 1000 milliseconds
+    randomAng = static_cast <float> (rand()) / static_cast <float> (RAND_MAX); // generate a number from 0.0 to 1.0 inclusively
+    glutPostRedisplay();
+}
+// print char array
+void printingOut(char charAr[])
+{
+    size_t i = 0;
+    while(i <= strlen(charAr) ){
+        std::cout << charAr[i];
         i++;
     }
 
@@ -83,17 +85,17 @@ void printingOut(char name[])
 void myIdle(void)
 {
     myDisplay();
-    FPS = CalFrequency();
-    char header[] = "FPS = ";
-    char buffer[64];
-    int ret = snprintf(buffer, sizeof buffer, "%f", FPS);
-    strcpy(fpsChar,header);
-    if(FPS != FPS_old){
-        strcat(fpsChar,buffer);
-        printingOut(fpsChar);
-        FPS_old = FPS;
-    }
-    strcat(fpsChar,buffer);
+    // FPS = CalFrequency();
+    // char header[] = "FPS = ";
+    // char buffer[64];
+    // int ret = snprintf(buffer, sizeof buffer, "%f", FPS);
+    // strcpy(fpsChar,header);
+    // if(FPS != FPS_old){
+    //     strcat(fpsChar,buffer);
+    //     printingOut(fpsChar);
+    //     FPS_old = FPS;
+    // }
+    // strcat(fpsChar,buffer);
 }
 
 double CalFrequency(void)
@@ -114,6 +116,7 @@ double CalFrequency(void)
      save = 50.0/timegap;
      return save;
 }
+
 void print(int x, int y,int z, char *string)
 {
     //set the position of the text in the window using the x and y coordinates
@@ -126,31 +129,34 @@ void print(int x, int y,int z, char *string)
     {
         glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24,string[i]);
     }
-};
+}
+
 void print_bitmap_string(char* s)
 {
-   if (s && strlen(s)) {
-      while (*s) {
-         glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_10, *s);
-         s++;
-      }
-   }
+    glViewport(0,0,WIDTH,HEIGHT);
+    float x = -225.0,y = 70.0;
+    glRasterPos2f(x, y);
+    print_bitmap_string(fpsChar);
+    if (s && strlen(s)) {
+        while (*s) {
+            glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_10, *s);
+            s++;
+        }
+    }
 }
+
 int main(int argc, char* argv[])
 {
 
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
-    glutInitWindowPosition(100, 100);
+    // glutInitWindowPosition(100, 100);
     glutInitWindowSize(WIDTH, HEIGHT);
-    glutCreateWindow("First OpenGL Example");
+    glutCreateWindow("First Radar Example");
     glutDisplayFunc(&myDisplay);
     glutIdleFunc(&myIdle);
+    glutTimerFunc(30, &drive, -1);
     glutMainLoop();
-    glViewport(0,0,WIDTH,HEIGHT);
-    float x = -225.0,y = 70.0;
-    glRasterPos2f(x, y);
-    print_bitmap_string(fpsChar);
     return 0;
 
 }
